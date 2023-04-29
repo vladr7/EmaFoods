cbe75c0e8f9c8fab157012ce3a1c6a59aa02fadf
Vlad <ricean.vlad7@gmail.com>
2023-04-29 10:55:17 +0300

    Empty generate food screen

---
+++ app/src/main/java/com/example/emafoods/feature/generatefood/GenerateScreen.kt
@@ -0,0 +1,20 @@
+package com.example.emafoods.feature.generatefood
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun GenerateScreenRoute(
+    modifier: Modifier = Modifier
+) {
+    GenerateScreen(
+        modifier = modifier
+    )
+}
+
+@Composable
+fun GenerateScreen(
+    modifier: Modifier = Modifier
+) {
+
+}
