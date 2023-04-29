f37b56560b985152e532eeabb0095645b28a346e
Vlad <ricean.vlad7@gmail.com>
2023-04-29 11:32:02 +0300

    Dummy usecase

--- app/src/main/java/com/example/emafoods/feature/generatefood/presentation/GenerateViewModel.kt
+++ app/src/main/java/com/example/emafoods/feature/generatefood/presentation/GenerateViewModel.kt
@@ -3,18 +3,30 @@ package com.example.emafoods.feature.generatefood.presentation
 import androidx.lifecycle.ViewModel
 import com.example.emafoods.core.presentation.models.FoodMapper
 import com.example.emafoods.core.presentation.models.FoodViewData
+import com.example.emafoods.feature.generatefood.domain.GenerateFoodUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
 class GenerateViewModel @Inject constructor(
     private val foodMapper: FoodMapper,
+    private val generateFoodUseCase: GenerateFoodUseCase,
 ) : ViewModel() {
 
     private val _state = MutableStateFlow<GenerateViewState>(GenerateViewState())
     val state: StateFlow<GenerateViewState> = _state
+
+    fun generateFoodEvent() {
+        val food = generateFoodUseCase.execute()
+        _state.update {
+            it.copy(
+                food = foodMapper.mapToViewData(food)
+            )
+        }
+    }
 }
 
 data class GenerateViewState(
