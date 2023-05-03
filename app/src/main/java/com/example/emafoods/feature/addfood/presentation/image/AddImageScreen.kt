415bf0824cc6cc888e66406f372977c7d2a22933
Vlad <ricean.vlad7@gmail.com>
2023-05-03 10:15:08 +0300

    Extended icons & background for ImageScreen

--- app/src/main/java/com/example/emafoods/feature/addfood/presentation/image/AddImageScreen.kt
+++ app/src/main/java/com/example/emafoods/feature/addfood/presentation/image/AddImageScreen.kt
@@ -6,6 +6,7 @@ import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -15,6 +16,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardActions
@@ -22,17 +24,29 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AttachFile
+import androidx.compose.material.icons.filled.PhotoCamera
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -76,10 +90,69 @@ fun AddImageScreen(
     foodDescription: String,
     onNextClick: () -> Unit,
 ) {
-    Button(onClick = {
-        onNextClick()
-    }) {
-        Text(text = "Add food (Image screen)")
+    AddImageScreenBackground()
+
+    Box(
+        modifier = modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.BottomEnd
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp),
+        ) {
+            Icon(
+                imageVector = Icons.Filled.AttachFile, contentDescription = null,
+                modifier = Modifier
+                    .padding(16.dp)
+                    .size(150.dp)
+            )
+            Icon(
+                imageVector = Icons.Filled.PhotoCamera,
+                contentDescription = null,
+                modifier = Modifier
+                    .padding(16.dp)
+                    .size(150.dp)
+            )
+        }
+    }
+
+
+}
+
+@Composable
+fun AddImageScreenBackground(
+    modifier: Modifier = Modifier
+) {
+    var sizeImage by remember { mutableStateOf(IntSize.Zero) }
+
+    val gradient = Brush.verticalGradient(
+        colors = listOf(
+            Color.Transparent,
+            androidx.compose.material3.MaterialTheme.colorScheme.secondary
+        ),
+        startY = sizeImage.height.toFloat(),
+        endY = sizeImage.height.toFloat() / 4,
+    )
+
+    Box() {
+        Image(
+            painter = painterResource(id = R.drawable.background),
+            contentDescription = null,
+            contentScale = ContentScale.FillHeight,
+            modifier = modifier
+                .onGloballyPositioned {
+                    sizeImage = it.size
+                }
+                .fillMaxSize(),
+            alpha = 0.2f
+        )
+        Box(
+            modifier = Modifier
+                .matchParentSize()
+                .alpha(0.7f)
+                .background(gradient)
+        )
     }
 }
 
@@ -94,7 +167,7 @@ fun AddFoodScreenTemp(
     imageUri: Uri? = null,
     foodTitle: String,
     foodDescription: String,
-    ) {
+) {
 
     if (errorMessage != null) {
         Toast.makeText(context, errorMessage, Toast.LENGTH_SHORT).show()
@@ -142,7 +215,10 @@ fun AddFoodScreenTemp(
                     androidx.compose.material.Text(text = "Camera")
                 }
 
-                androidx.compose.material.Text("sau", modifier = Modifier.padding(top = 8.dp, bottom = 8.dp))
+                androidx.compose.material.Text(
+                    "sau",
+                    modifier = Modifier.padding(top = 8.dp, bottom = 8.dp)
+                )
 
                 Button(onClick = {
                     imagePicker.launch("image/*")
