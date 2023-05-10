7d0d1d8599f33ae1a26679dd26db8562d674b983
Vlad <ricean.vlad7@gmail.com>
2023-05-10 09:43:36 +0300

    Add select/ take picture components on top right image

--- app/src/main/java/com/example/emafoods/feature/addfood/presentation/insert/InsertFoodViewModel.kt
+++ app/src/main/java/com/example/emafoods/feature/addfood/presentation/insert/InsertFoodViewModel.kt
@@ -6,6 +6,7 @@ import com.example.emafoods.core.presentation.base.ViewState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
@@ -18,16 +19,18 @@ class InsertFoodViewModel @Inject constructor(
     )
     val state: StateFlow<InsertFoodViewState> = _state
 
-    fun updateTitle(title: String) {
-        if (title.length > 4) {
-            _state.value = _state.value.copy(showNextButton = true, title = title)
-        } else {
-            _state.value = _state.value.copy(showNextButton = false, title = title)
-        }
-    }
-
     fun updateDescription(description: String) {
         _state.value = _state.value.copy(description = description)
+    }
+
+    fun updateImageUri(uri: Uri?) {
+        if(uri != null) {
+            _state.value = _state.value.copy(imageUri = uri)
+        } else {
+            _state.update {
+                it.copy(errorMessage = "Please select an image")
+            }
+        }
     }
 }
 
@@ -35,7 +38,6 @@ data class InsertFoodViewState(
     override val isLoading: Boolean = false,
     override val errorMessage: String? = null,
     val imageUri: Uri? = null,
-    val title: String = "",
     val description: String = "",
     val showNextButton: Boolean = false,
 ) : ViewState(isLoading, errorMessage)
