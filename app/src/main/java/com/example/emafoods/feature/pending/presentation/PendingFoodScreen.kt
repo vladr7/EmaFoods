66deed91ab7565c1057d015e8a9d956f3764b2b8
Vlad <ricean.vlad7@gmail.com>
2023-05-14 08:38:07 +0300

    Add swipe functionality

--- app/src/main/java/com/example/emafoods/feature/pending/presentation/PendingFoodScreen.kt
+++ app/src/main/java/com/example/emafoods/feature/pending/presentation/PendingFoodScreen.kt
@@ -1,24 +1,39 @@
 package com.example.emafoods.feature.pending.presentation
 
+import android.widget.Toast
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.draggable
+import androidx.compose.foundation.gestures.rememberDraggableState
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.LunchDining
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
@@ -36,6 +51,7 @@ import coil.compose.SubcomposeAsyncImage
 import coil.request.ImageRequest
 import com.example.emafoods.R
 import com.example.emafoods.core.presentation.models.FoodViewData
+import kotlinx.coroutines.launch
 
 @Composable
 fun PendingFoodRoute(
@@ -48,6 +64,14 @@ fun PendingFoodRoute(
     PendingFoodScreen(
         modifier = modifier,
         food = state.food,
+        onSwipeLeft = {
+            viewModel.onSwipeLeft()
+            Toast.makeText(context, "Delete", Toast.LENGTH_SHORT).show()
+        },
+        onSwipeRight = {
+            Toast.makeText(context, "Accept", Toast.LENGTH_SHORT).show()
+            viewModel.onSwipeRight()
+        }
     )
 }
 
@@ -55,10 +79,26 @@ fun PendingFoodRoute(
 fun PendingFoodScreen(
     modifier: Modifier = Modifier,
     food: FoodViewData,
+    onSwipeLeft: () -> Unit,
+    onSwipeRight: () -> Unit,
 ) {
-    FoodItem(
-        food = food,
-    )
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        FoodItem(
+            food = food,
+            modifier = modifier,
+        )
+        Spacer(modifier = Modifier.weight(1f))
+        PendingSwipe(
+            modifier = modifier,
+            onSwipeRight = onSwipeRight,
+            onSwipeLeft = onSwipeLeft,
+        )
+        Spacer(modifier = Modifier.weight(1f))
+    }
+
 }
 
 @Composable
@@ -91,6 +131,93 @@ fun FoodItem(
             PendingFoodAuthor(author = food.author)
         }
     }
+}
+
+@Composable
+fun PendingSwipe(
+    modifier: Modifier = Modifier,
+    onSwipeRight: () -> Unit,
+    onSwipeLeft: () -> Unit,
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(26.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        RejectFood()
+        SwipableFood(
+            onSwipeLeft = onSwipeLeft,
+            onSwipeRight = onSwipeRight,
+        )
+        AcceptFood()
+    }
+}
+
+@Composable
+fun RejectFood() {
+    Icon(
+        Icons.Filled.Delete, contentDescription = "Reject Food",
+        modifier = Modifier
+            .height(50.dp)
+            .width(50.dp)
+    )
+}
+
+@Composable
+fun SwipableFood(
+    modifier: Modifier = Modifier,
+    onSwipeLeft: () -> Unit,
+    onSwipeRight: () -> Unit,
+) {
+
+    val coroutineScope = rememberCoroutineScope()
+    val offsetX = remember { Animatable(0f) }
+    println("vlad: ${offsetX.value}")
+
+    Icon(
+        Icons.Filled.LunchDining, contentDescription = "Swipable Food",
+        modifier = Modifier
+            .offset(
+                x = (offsetX.value.toInt() * 2).dp,
+            )
+            .draggable(
+                state = rememberDraggableState { delta ->
+                    coroutineScope.launch {
+                        offsetX.animateTo(offsetX.value + delta)
+                    }
+                },
+                orientation = Orientation.Horizontal,
+                onDragStarted = {
+
+                },
+                onDragStopped = {
+                    if ((offsetX.value.toInt() * 2) < -80f) {
+                        onSwipeLeft()
+                    }
+                    if ((offsetX.value.toInt() * 2) > 80f) {
+                        onSwipeRight()
+                    }
+                    coroutineScope.launch {
+                        offsetX.animateTo(0f)
+                    }
+                }
+            )
+            .padding(70.dp)
+            .height(50.dp)
+            .width(50.dp)
+    )
+}
+
+@Composable
+fun AcceptFood() {
+    Icon(
+        Icons.Filled.Favorite, contentDescription = "Accept Food",
+        modifier = Modifier
+            .height(50.dp)
+            .width(50.dp)
+    )
 }
 
 @OptIn(ExperimentalTextApi::class)
