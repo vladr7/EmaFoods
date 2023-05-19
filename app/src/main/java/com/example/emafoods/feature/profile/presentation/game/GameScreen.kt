a1a1b5d4bf3948f3b08a1744a25450ca005f0cb5
Vlad <ricean.vlad7@gmail.com>
2023-05-19 08:04:01 +0300

    Game header

--- app/src/main/java/com/example/emafoods/feature/profile/presentation/game/GameScreen.kt
+++ app/src/main/java/com/example/emafoods/feature/profile/presentation/game/GameScreen.kt
@@ -1,7 +1,40 @@
 package com.example.emafoods.feature.profile.presentation.game
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawWithCache
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.dp
+import com.example.emafoods.R
 
 @Composable
 fun GameRoute(
@@ -9,12 +42,123 @@ fun GameRoute(
 ) {
     GameScreen(
         modifier = modifier,
+        userName = "User Name",
+        level = "Nivel 1"
     )
 }
 
 @Composable
 fun GameScreen(
+    modifier: Modifier = Modifier,
+    userName: String,
+    level: String
+) {
+    GameBackground()
+    Column(
+        modifier = modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        GameHeader(
+            modifier = modifier,
+            userName = userName,
+            level = level
+        )
+    }
+
+}
+
+@Composable
+fun GameHeader(
+    modifier: Modifier,
+    userName: String,
+    level: String
+) {
+    Row(
+        modifier = modifier
+            .padding(start = 16.dp, end = 16.dp, top = 36.dp, bottom = 16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.profilepic1),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(100.dp)
+                .clip(CircleShape)
+        )
+        Column(
+            modifier = Modifier
+                .padding(start = 16.dp)
+                .align(Alignment.CenterVertically)
+        ) {
+            Text(
+                text = level,
+                style = MaterialTheme.typography.headlineLarge,
+                color = MaterialTheme.colorScheme.onSecondary
+            )
+            Row {
+                Text(
+                    text = userName,
+                    style = MaterialTheme.typography.bodyLarge,
+                    color = MaterialTheme.colorScheme.onSecondary
+                )
+                Icon(
+                    imageVector = Icons.Filled.Favorite,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .padding(start = 4.dp)
+                        .graphicsLayer(alpha = 0.99f)
+                        .drawWithCache {
+                            onDrawWithContent {
+                                drawContent()
+                                drawRect(
+                                    Brush.horizontalGradient(
+                                        listOf(
+                                            Color.Red,
+                                            Color.White
+                                        )
+                                    ), blendMode = BlendMode.SrcAtop
+                                )
+                            }
+                        },
+                )
+            }
+
+        }
+    }
+}
+
+@Composable
+fun GameBackground(
     modifier: Modifier = Modifier
 ) {
+    var sizeImage by remember { mutableStateOf(IntSize.Zero) }
 
+    val gradient = Brush.verticalGradient(
+        colors = listOf(MaterialTheme.colorScheme.secondary, MaterialTheme.colorScheme.secondary),
+        startY = sizeImage.height.toFloat(),
+        endY = 0f
+    )
+
+    Box() {
+        Image(
+            painter = painterResource(id = R.drawable.profilebackground),
+            contentDescription = null,
+            contentScale = ContentScale.FillHeight,
+            modifier = modifier
+                .onGloballyPositioned {
+                    sizeImage = it.size
+                }
+                .fillMaxSize()
+                .alpha(0.5f),
+        )
+        Box(
+            modifier = Modifier
+                .matchParentSize()
+                .alpha(0.9f)
+                .background(gradient)
+        )
+    }
 }
