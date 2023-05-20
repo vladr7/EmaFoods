aef5853e1264b3de630de1237b58c98313b14cfd
Vlad <ricean.vlad7@gmail.com>
2023-05-20 18:25:30 +0300

    Add to DI

--- app/src/main/java/com/example/emafoods/feature/profile/di/ProfileModule.kt
+++ app/src/main/java/com/example/emafoods/feature/profile/di/ProfileModule.kt
@@ -5,9 +5,14 @@ import com.example.emafoods.core.domain.network.AuthService
 import com.example.emafoods.feature.profile.data.datasource.DefaultGameDataSource
 import com.example.emafoods.feature.profile.data.datasource.GameDataSource
 import com.example.emafoods.feature.profile.data.repository.DefaultGameRepository
+import com.example.emafoods.feature.profile.domain.mapper.MapLevelPermissionToViewData
 import com.example.emafoods.feature.profile.domain.repository.GameRepository
+import com.example.emafoods.feature.profile.domain.usecase.GetLevelPermissionsUseCase
 import com.example.emafoods.feature.profile.domain.usecase.GetListOfXpActionsUseCase
+import com.example.emafoods.feature.profile.domain.usecase.GetNextLevelUseCase
+import com.example.emafoods.feature.profile.domain.usecase.GetUserGameDetailsUseCase
 import com.example.emafoods.feature.profile.domain.usecase.SignOutUseCase
+import com.example.emafoods.feature.profile.domain.usecase.StoreUserLevelUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -48,5 +53,45 @@ object ProfileModule {
         gameRepository: GameRepository
     ) = GetListOfXpActionsUseCase(
         gameRepository = gameRepository
+    )
+
+    @Provides
+    @Singleton
+    fun provideGetLevelPermissionsUseCase(
+        gameRepository: GameRepository
+    ) = GetLevelPermissionsUseCase(
+        gameRepository = gameRepository
+    )
+
+    @Provides
+    @Singleton
+    fun provideGetNextLevelUseCase(
+        getUserGameDetailsUseCase: GetUserGameDetailsUseCase
+    ) = GetNextLevelUseCase(
+        getUserGameDetailsUseCase = getUserGameDetailsUseCase
+    )
+
+    @Provides
+    @Singleton
+    fun provideGetUserGameDetailsUseCase(
+        gameRepository: GameRepository
+    ) = GetUserGameDetailsUseCase(
+        gameRepository
+    )
+
+    @Provides
+    @Singleton
+    fun provideStoreUserLevelUseCase(
+        gameRepository: GameRepository
+    ) = StoreUserLevelUseCase(
+        gameRepository
+    )
+
+    @Provides
+    @Singleton
+    fun provideMapLevelPermissionToViewData(
+        getUserGameDetailsUseCase: GetUserGameDetailsUseCase
+    ) = MapLevelPermissionToViewData(
+        getUserGameDetailsUseCase = getUserGameDetailsUseCase
     )
 }
