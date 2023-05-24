92086a62dfecf7670550757b94b678e3d50ba7fb
Vlad <ricean.vlad7@gmail.com>
2023-05-24 07:47:05 +0300

    Hide pending from low level

--- app/src/main/java/com/example/emafoods/MainViewModel.kt
+++ app/src/main/java/com/example/emafoods/MainViewModel.kt
@@ -1,15 +1,20 @@
 package com.example.emafoods
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.emafoods.core.domain.network.AuthService
+import com.example.emafoods.feature.game.domain.model.UserLevel
+import com.example.emafoods.feature.game.domain.usecase.GetUserGameDetailsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    private val authService: AuthService
+    private val authService: AuthService,
+    private val getUserGameDetailsUseCase: GetUserGameDetailsUseCase
 ): ViewModel() {
 
     private val _state = MutableStateFlow<MainViewState>(MainViewState())
@@ -17,6 +22,16 @@ class MainViewModel @Inject constructor(
 
     init {
         checkIfUserIsSignedIn()
+        checkUserLevel()
+    }
+
+    private fun checkUserLevel() {
+        viewModelScope.launch {
+            val userLevel = getUserGameDetailsUseCase.execute().userLevel
+            _state.value = _state.value.copy(
+                userLevel = userLevel
+            )
+        }
     }
 
     private fun checkIfUserIsSignedIn() {
@@ -28,4 +43,5 @@ class MainViewModel @Inject constructor(
 
 data class MainViewState(
     val isUserSignedIn: Boolean = false,
+    val userLevel: UserLevel = UserLevel.LEVEL_1
 )
--- app/src/main/java/com/example/emafoods/feature/game/domain/model/UserLevel.kt
+++ app/src/main/java/com/example/emafoods/feature/game/domain/model/UserLevel.kt
@@ -1,11 +1,11 @@
 package com.example.emafoods.feature.game.domain.model
 
 enum class UserLevel(val string: String, val xp: Int) {
-    LEVEL_1(string = "Level 1", xp = 0), // pleb
-    LEVEL_2(string = "Level 2", xp = 10000), // read all list
-    LEVEL_3(string = "Level 3", xp = 20000), // add/ decline
-    LEVEL_4(string = "Level 4", xp = 30000), // edit pending food before added
-    LEVEL_5(string = "Level 5", xp = 100000),;  // suprize
+    LEVEL_1(string = "Nivel 1", xp = 0), // pleb
+    LEVEL_2(string = "Nivel 2", xp = 10000), // read all list
+    LEVEL_3(string = "Nivel 3", xp = 20000), // add/ decline
+    LEVEL_4(string = "Nivel 4", xp = 30000), // edit pending food before added
+    LEVEL_5(string = "Nivel 5", xp = 100000),;  // suprize
 
     companion object {
         fun fromString(userLevelString: String): UserLevel =
