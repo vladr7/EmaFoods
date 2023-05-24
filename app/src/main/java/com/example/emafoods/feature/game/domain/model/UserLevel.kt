6cf0e42d0359daf05813a0a2959ca240c8ba6506
Vlad <ricean.vlad7@gmail.com>
2023-05-24 07:31:20 +0300

    Organized proper levels

--- app/src/main/java/com/example/emafoods/feature/game/domain/model/UserLevel.kt
+++ app/src/main/java/com/example/emafoods/feature/game/domain/model/UserLevel.kt
@@ -2,10 +2,10 @@ package com.example.emafoods.feature.game.domain.model
 
 enum class UserLevel(val string: String, val xp: Int) {
     LEVEL_1(string = "Level 1", xp = 0), // pleb
-    LEVEL_2(string = "Level 2", xp = 10000), // add/ decline
-    LEVEL_3(string = "Level 3", xp = 20000), // read all list
-    LEVEL_4(string = "Level 4", xp = 30000), // edit list
-    LEVEL_5(string = "Level 5", xp = 100000),;  // full access
+    LEVEL_2(string = "Level 2", xp = 10000), // read all list
+    LEVEL_3(string = "Level 3", xp = 20000), // add/ decline
+    LEVEL_4(string = "Level 4", xp = 30000), // edit pending food before added
+    LEVEL_5(string = "Level 5", xp = 100000),;  // suprize
 
     companion object {
         fun fromString(userLevelString: String): UserLevel =
