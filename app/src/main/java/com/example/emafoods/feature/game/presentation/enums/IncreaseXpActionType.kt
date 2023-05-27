cd7d1d1182fa25cfd146d3803bd65e0894e1ed7a
Vlad <ricean.vlad7@gmail.com>
2023-05-27 07:33:16 +0300

    Fix recipe accepted rewards xp

--- app/src/main/java/com/example/emafoods/feature/game/presentation/enums/IncreaseXpActionType.kt
+++ app/src/main/java/com/example/emafoods/feature/game/presentation/enums/IncreaseXpActionType.kt
@@ -4,7 +4,7 @@ enum class IncreaseXpActionType(val xp: Int) {
     ADD_RECIPE(xp = 50),
     ADD_REVIEW(xp = 1000),
     GENERATE_RECIPE(xp = 10000),
-    RECIPE_ACCEPTED(xp = 1500),
+    RECIPE_ACCEPTED(xp = 15000),
     ADMIN_ACCEPTS_DECLINES_RECIPE(xp = 50),
 }
 
