37355a0aeab53eab7964d1cb09f41e163072c09c
Vlad <ricean.vlad7@gmail.com>
2023-05-27 14:57:22 +0300

    Inform user that recipe has been added to waiting list

--- app/src/main/java/com/example/emafoods/feature/addfood/presentation/congratulation/CongratulationViewModel.kt
+++ app/src/main/java/com/example/emafoods/feature/addfood/presentation/congratulation/CongratulationViewModel.kt
@@ -2,6 +2,8 @@ package com.example.emafoods.feature.addfood.presentation.congratulation
 
 import com.example.emafoods.core.presentation.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
@@ -9,5 +11,22 @@ class CongratulationViewModel @Inject constructor(
 
 ): BaseViewModel() {
 
+    private val _state = MutableStateFlow<CongratulationViewState>(CongratulationViewState())
+    val state: StateFlow<CongratulationViewState> = _state
 
+    init {
+        _state.value = CongratulationViewState(
+            showMessages = true
+        )
+    }
+
+    fun onMessagesShown() {
+        _state.value = CongratulationViewState(
+            showMessages = false
+        )
+    }
 }
+
+data class CongratulationViewState(
+    val showMessages: Boolean = false,
+)
