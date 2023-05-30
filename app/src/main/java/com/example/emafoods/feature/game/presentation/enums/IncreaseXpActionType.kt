3d6fdf64db360ade4b0edd12ac4c2cd129795682
Vlad <ricean.vlad7@gmail.com>
2023-05-30 19:09:24 +0300

    Reduce generate xp

--- app/src/main/java/com/example/emafoods/feature/game/presentation/enums/IncreaseXpActionType.kt
+++ app/src/main/java/com/example/emafoods/feature/game/presentation/enums/IncreaseXpActionType.kt
@@ -3,7 +3,7 @@ package com.example.emafoods.feature.game.presentation.enums
 enum class IncreaseXpActionType(val xp: Long) {
     ADD_RECIPE(xp = 50),
     ADD_REVIEW(xp = 1000),
-    GENERATE_RECIPE(xp = 10000),
+    GENERATE_RECIPE(xp = 5),
     RECIPE_ACCEPTED(xp = 1500),
     ADMIN_ACCEPTS_DECLINES_RECIPE(xp = 50),
 }
