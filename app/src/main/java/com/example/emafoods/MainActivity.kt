f92f9d5170fb872d3d750ac057ebdfa4334d29f9
Vlad <ricean.vlad7@gmail.com>
2023-05-30 06:45:39 +0300

    Immediate update

--- app/src/main/java/com/example/emafoods/MainActivity.kt
+++ app/src/main/java/com/example/emafoods/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.emafoods
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -14,12 +15,22 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.emafoods.navigation.home.EmaFoodsNavigation
 import com.example.emafoods.navigation.signin.SignInNavigation
 import com.example.emafoods.ui.theme.EmaTheme
+import com.google.android.play.core.appupdate.AppUpdateManager
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.install.model.AppUpdateType
+import com.google.android.play.core.install.model.UpdateAvailability
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private lateinit var appUpdateManager: AppUpdateManager
+    private val updateType = AppUpdateType.IMMEDIATE
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        appUpdateManager = AppUpdateManagerFactory.create(applicationContext)
+        checkForAppUpdate()
 
         WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
@@ -34,15 +45,17 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    when(state.value.userSignInState) {
+                    when (state.value.userSignInState) {
                         UserSignInState.LOADING -> {
 //                            LoadingScreen()
                         }
+
                         UserSignInState.SIGNED_IN -> {
                             EmaFoodsNavigation(
                                 isAdmin = state.value.isAdmin
                             )
                         }
+
                         UserSignInState.NOT_SIGNED_IN -> {
                             SignInNavigation()
                         }
@@ -50,6 +63,37 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+    private fun checkForAppUpdate() {
+        appUpdateManager.appUpdateInfo.addOnSuccessListener { appUpdateInfo ->
+            val isUpdateAvailable =
+                appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
+            val isUpdateTypeAllowed = appUpdateInfo.isUpdateTypeAllowed(updateType)
+            if (isUpdateAvailable && isUpdateTypeAllowed) {
+                appUpdateManager.startUpdateFlowForResult(
+                    appUpdateInfo,
+                    updateType,
+                    this,
+                    APP_UPDATE_REQUEST_CODE
+                )
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == APP_UPDATE_REQUEST_CODE) {
+            if (resultCode != RESULT_OK) {
+                println("Update flow failed! Result code: $resultCode")
+            } else {
+                println("Update flow complete! Result code: $resultCode")
+            }
+        }
+    }
+
+    companion object {
+        private const val APP_UPDATE_REQUEST_CODE = 9001
     }
 }
 
