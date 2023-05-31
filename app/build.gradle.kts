eccd1d54b4525a097e341fb53c98f6cfbf9cfc23
Vlad <ricean.vlad7@gmail.com>
2023-05-31 08:24:45 +0300

    Increase version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         applicationId = "com.riviem.emafoods"
         minSdk = 26
         targetSdk = 33
-        versionCode = (findProperty("versionCode") as? String)?.toIntOrNull() ?: 1
+        versionCode = 3
         versionName = libs.versions.versionName.get()
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
