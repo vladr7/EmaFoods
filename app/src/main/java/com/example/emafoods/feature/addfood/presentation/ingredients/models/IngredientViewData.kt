51510697641bee422ea715644636ecea92a82f8a
Vlad <ricean.vlad7@gmail.com>
2023-06-10 13:43:29 +0300

    Add id

--- app/src/main/java/com/example/emafoods/feature/addfood/presentation/ingredients/models/IngredientViewData.kt
+++ app/src/main/java/com/example/emafoods/feature/addfood/presentation/ingredients/models/IngredientViewData.kt
@@ -3,6 +3,7 @@ package com.example.emafoods.feature.addfood.presentation.ingredients.models
 import com.example.emafoods.core.presentation.models.helper.ViewData
 
 data class IngredientViewData(
+    val id: Long = -1,
     val name: String,
     val measurement: Long
 ): ViewData()
