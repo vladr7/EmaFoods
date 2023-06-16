b884b66261344cb2083cfbca997ba5b3ed6745fc
Vlad <ricean.vlad7@gmail.com>
2023-06-16 07:58:17 +0300

    Refactor generate/previous logic (hide button on first generate)

--- app/src/main/java/com/example/emafoods/feature/generatefood/domain/usecase/GenerateFoodUseCase.kt
+++ app/src/main/java/com/example/emafoods/feature/generatefood/domain/usecase/GenerateFoodUseCase.kt
@@ -1,68 +1,16 @@
 package com.example.emafoods.feature.generatefood.domain.usecase
 
 import com.example.emafoods.core.presentation.models.FoodViewData
-import com.example.emafoods.feature.addfood.presentation.category.CategoryType
-import com.example.emafoods.feature.generatefood.domain.models.GenerateResult
 import javax.inject.Inject
 
 class GenerateFoodUseCase @Inject constructor() {
 
-    fun execute(
-        categoryType: CategoryType,
-        mainDishList: List<FoodViewData>,
-        dessertList: List<FoodViewData>,
-        breakfastList: List<FoodViewData>,
-        soupList: List<FoodViewData>,
-        indexMainDish: Int,
-        indexDessert: Int,
-        indexBreakfast: Int,
-        indexSoup: Int
-    ): GenerateResult<FoodViewData> {
-        return when (categoryType) {
-            CategoryType.MAIN_DISH -> {
-                if(mainDishList.isEmpty()) {
-                    GenerateResult.ErrorEmptyList(Unit)
-                } else {
-                    if(indexMainDish == mainDishList.size - 1) {
-                        GenerateResult.SuccessAndIndexMustBeReset(mainDishList[0])
-                    } else {
-                        GenerateResult.Success(mainDishList[indexMainDish + 1])
-                    }
-                }
-            }
-            CategoryType.DESSERT -> {
-                if (dessertList.isEmpty()) {
-                    GenerateResult.ErrorEmptyList(Unit)
-                } else {
-                    if(indexDessert == dessertList.size - 1) {
-                        GenerateResult.SuccessAndIndexMustBeReset(dessertList[0])
-                    } else {
-                        GenerateResult.Success(dessertList[indexDessert + 1])
-                    }
-                }
-            }
-            CategoryType.BREAKFAST -> {
-                if(breakfastList.isEmpty()) {
-                    GenerateResult.ErrorEmptyList(Unit)
-                } else {
-                    if(indexBreakfast == breakfastList.size - 1) {
-                        GenerateResult.SuccessAndIndexMustBeReset(breakfastList[0])
-                    } else {
-                        GenerateResult.Success(breakfastList[indexBreakfast + 1])
-                    }
-                }
-            }
-            CategoryType.SOUP -> {
-                if(soupList.isEmpty()) {
-                    GenerateResult.ErrorEmptyList(Unit)
-                } else {
-                    if(indexSoup == soupList.size - 1) {
-                        GenerateResult.SuccessAndIndexMustBeReset(soupList[0])
-                    } else {
-                        GenerateResult.Success(soupList[indexSoup + 1])
-                    }
-                }
-            }
+    fun execute(foodList: List<FoodViewData>, index: Int): FoodViewData {
+        if(index >= foodList.size - 1) {
+            return foodList[0]
         }
+        return foodList[index + 1]
     }
+
+
 }
--- app/src/main/java/com/example/emafoods/feature/generatefood/domain/usecase/PreviousFoodUseCase.kt
+++ app/src/main/java/com/example/emafoods/feature/generatefood/domain/usecase/PreviousFoodUseCase.kt
@@ -1,66 +1,14 @@
 package com.example.emafoods.feature.generatefood.domain.usecase
 
 import com.example.emafoods.core.presentation.models.FoodViewData
-import com.example.emafoods.feature.addfood.presentation.category.CategoryType
-import com.example.emafoods.feature.generatefood.domain.models.PreviousFoodResult
 import javax.inject.Inject
 
 class PreviousFoodUseCase @Inject constructor() {
-    fun execute(
-        categoryType: CategoryType,
-        mainDishList: List<FoodViewData>,
-        dessertList: List<FoodViewData>,
-        breakfastList: List<FoodViewData>,
-        soupList: List<FoodViewData>,
-        indexMainDish: Int,
-        indexDessert: Int,
-        indexBreakfast: Int,
-        indexSoup: Int
-    ): PreviousFoodResult<FoodViewData> =
-        when (categoryType) {
-            CategoryType.MAIN_DISH -> {
-                if (mainDishList.isEmpty()) {
-                    PreviousFoodResult.ErrorEmptyList(Unit)
-                } else {
-                    if (indexMainDish <= 1) {
-                        PreviousFoodResult.SuccessAndStartOfTheList(mainDishList[0])
-                    } else {
-                        PreviousFoodResult.Success(mainDishList[indexMainDish - 1])
-                    }
-                }
-            }
-            CategoryType.DESSERT -> {
-                if (dessertList.isEmpty()) {
-                    PreviousFoodResult.ErrorEmptyList(Unit)
-                } else {
-                    if (indexDessert <= 1) {
-                        PreviousFoodResult.SuccessAndStartOfTheList(dessertList[0])
-                    } else {
-                        PreviousFoodResult.Success(dessertList[indexDessert - 1])
-                    }
-                }
-            }
-            CategoryType.BREAKFAST -> {
-                if (breakfastList.isEmpty()) {
-                    PreviousFoodResult.ErrorEmptyList(Unit)
-                } else {
-                    if (indexBreakfast <= 1) {
-                        PreviousFoodResult.SuccessAndStartOfTheList(breakfastList[0])
-                    } else {
-                        PreviousFoodResult.Success(breakfastList[indexBreakfast - 1])
-                    }
-                }
-            }
-            CategoryType.SOUP -> {
-                if (soupList.isEmpty()) {
-                    PreviousFoodResult.ErrorEmptyList(Unit)
-                } else {
-                    if (indexSoup <= 1) {
-                        PreviousFoodResult.SuccessAndStartOfTheList(soupList[0])
-                    } else {
-                        PreviousFoodResult.Success(soupList[indexSoup - 1])
-                    }
-                }
-            }
+
+    fun execute(foodList: List<FoodViewData>, index: Int): FoodViewData {
+        if(index == 0) {
+            return foodList[foodList.size - 1]
         }
+        return foodList[index - 1]
+    }
 }
