ebde7d80b58f9aa5afe5293d7ce23899b313b158
Vlad <ricean.vlad7@gmail.com>
2023-06-17 19:08:35 +0300

    Level permissions

--- app/src/main/java/com/example/emafoods/feature/game/presentation/model/Permission.kt
+++ app/src/main/java/com/example/emafoods/feature/game/presentation/model/Permission.kt
@@ -3,8 +3,6 @@ package com.example.emafoods.feature.game.presentation.model
 enum class Permission(val value: String) {
     GENERATE("Generează rețete"),
     ADD_TO_PENDING("Adaugă o rețetă în lista de așteptare"),
-    ACCEPT_DENY_FROM_PENDING("Acceptă/Respinge rețete din lista de așteptare"),
-    MAIN_LIST_VISIBLE("Vizualizează întreaga listă de rețete"),
-    EDIT_PENDING("Editează reteta din lista de așteptare"),
+    MORE_AVATAR_ICONS("Iconițe suplimentare pentru alegerea avatarului"),
     SURPRISE("Surpriză!")
 }
