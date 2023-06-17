cda5d352a627c5abcb3480f04e4e5e47b186d9b9
Vlad <ricean.vlad7@gmail.com>
2023-06-17 19:08:35 +0300

    Level permissions

--- app/src/main/java/com/example/emafoods/feature/game/domain/usecase/CalculateUserLevelFromXpUseCase.kt
+++ app/src/main/java/com/example/emafoods/feature/game/domain/usecase/CalculateUserLevelFromXpUseCase.kt
@@ -11,9 +11,7 @@ class CalculateUserLevelFromXpUseCase @Inject constructor(
         when(xp) {
             in Int.MIN_VALUE until UserLevel.LEVEL_2.xp ->  UserLevel.LEVEL_1
             in UserLevel.LEVEL_2.xp until UserLevel.LEVEL_3.xp ->  UserLevel.LEVEL_2
-            in UserLevel.LEVEL_3.xp until UserLevel.LEVEL_4.xp ->  UserLevel.LEVEL_3
-            in UserLevel.LEVEL_4.xp until UserLevel.LEVEL_5.xp ->  UserLevel.LEVEL_4
-            in UserLevel.LEVEL_5.xp until Int.MAX_VALUE ->  UserLevel.LEVEL_5
+            in UserLevel.LEVEL_3.xp until Int.MAX_VALUE ->  UserLevel.LEVEL_3
             else -> UserLevel.LEVEL_1
         }
 }
