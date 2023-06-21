5b57520ade036338cbccabfbc9523524d41eec7c
Vlad <ricean.vlad7@gmail.com>
2023-06-21 17:25:38 +0300

    Release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         applicationId = "com.riviem.emafoods"
         minSdk = 26
         targetSdk = 33
-        versionCode = 3
+        versionCode = 4
         versionName = libs.versions.versionName.get()
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
