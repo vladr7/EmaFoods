0ed1919ac2536841b1d1ce23e0e67b4a105df7dd
Vlad <ricean.vlad7@gmail.com>
2023-06-28 18:45:24 +0300

    Lower height on button

--- app/src/main/java/com/example/emafoods/feature/addfood/presentation/image/ImageScreen.kt
+++ app/src/main/java/com/example/emafoods/feature/addfood/presentation/image/ImageScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -151,6 +152,7 @@ fun ImageScreen(
             Spacer(modifier = modifier.weight(0.5f))
             ImageScreenNextStep(
                 modifier
+                    .height(75.dp)
                     .weight(1f),
                 hasTakePictureImage,
                 onNextClicked,
